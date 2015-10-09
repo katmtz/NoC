f27916388650a6e919ec20635241bc4e6862e5a0
kmartine <katie.martinez95@gmail.com>
2015-10-08 23:03:25 -0400

    initial backup

---
+++ node.sv
@@ -0,0 +1,227 @@
+/*********************************************
+ *  18-341 Fall 2015                         *
+ *  Project 3                                *
+ *  Network-on-Chip node                     *
+ *********************************************/
+module node(clk, rst_b, pkt_in, pkt_in_avail, cQ_full, pkt_out, pkt_out_avail,
+            free_outbound, put_outbound, payload_outbound,
+            free_inbound, put_inbound, payload_inbound);
+
+  parameter NODEID = 0;
+  input clk, rst_b;
+
+  // Interface to TestBench
+  input pkt_t pkt_in;
+  input pkt_in_avail;
+  output cQ_full;
+  output pkt_t pkt_out;
+  output logic pkt_out_avail;
+
+  // Endpoint -> Router transaction
+  input free_outbound; // Router -> Endpoint
+  output put_outbound; // Endpoint -> Router
+  output [7:0] payload_outbound;
+
+  // Router -> Endpoint transaction
+  output free_inbound; // Endpoint -> Router
+  input put_inbound; // Router -> Endpoint
+  input [7:0] payload_inbound;
+
+  logic [31:0] q_out;
+  logic q_empty, q_ready, pts_ready;
+
+  assign q_ready = ~q_empty;
+
+  // TB --> NODE --> ROUTER
+  fifo        q       (.clk(clk), .rst_b(rst_b), 
+                       .data_in(pkt_in), .we(pkt_in_avail), 
+                       .data_out(q_out), .re(pts_ready),
+                       .full(cQ_full), .empty(q_empty));
+  pktToSerial toRouter(.clk(clk), .rst_b(rst_b), 
+                       .pkt(q_out), .pkt_avail(q_ready), 
+                       .payload_out(payload_outbound), .payload_avail(put_outbound), 
+                       .read_in(pts_ready), .req(free_outbound));
+
+  // ROUTER --> NODE --> TB
+  serialToPkt toTB    (.clk(clk), .rst_b(rst_b), 
+                       .payload(payload_inbound), .put(put_inbound),
+                       .pkt(pkt_out), .pkt_avail(pkt_out_avail),
+                       .free(free_inbound));
+endmodule
+
+module pktToSerial(clk, rst_b, pkt, pkt_avail, payload_out, payload_avail, read_in, req);
+  parameter WIDTH = 32;
+  input bit clk, rst_b;
+  input bit [31:0] pkt;
+  input bit pkt_avail, req;
+  output bit payload_avail, read_in;
+  output bit [7:0] payload_out;
+
+  reg [31:0] buff;
+  bit [31:0] buff_in;
+  logic [2:0] count;
+
+  pts_fsm fsm (.*);
+
+  always_comb begin
+    if (read_in) buff_in = pkt;
+    else buff_in = buff;
+  end
+
+  always_ff @(posedge clk, negedge rst_b) begin
+    if (~rst_b) buff <= 0;
+    else buff <= buff_in;
+  end
+
+  always_comb
+    case(count)
+      4: payload_out = buff[7:0];
+      3: payload_out = buff[15:8];
+      2: payload_out = buff[23:16];
+      1: payload_out = buff[31:24];
+      default: payload_out = 8'hcc;
+    endcase
+
+endmodule
+
+// pktToSerial fsm
+module pts_fsm(clk, rst_b, pkt_avail, payload_avail, req, read_in, count);
+  input bit clk, rst_b, pkt_avail, req;
+  output bit payload_avail, read_in;
+  output bit [2:0] count;
+
+  enum logic [1:0] {hold = 2'b00, load = 2'b01, send = 2'b10} state, nextState;
+
+  always_comb
+    case(state)
+      hold: nextState = (pkt_avail) ? load : hold;
+      load: nextState = (req) ? send : load;
+      send: nextState = (count == 4) ? (pkt_avail) ? load : hold : send;
+    endcase
+  
+  always_ff @(posedge clk, negedge rst_b) begin
+    if (~rst_b) state <= hold;
+    else state <= nextState;
+  end
+
+  reg [2:0] count_r;
+  logic [2:0] count_in;
+
+  always_comb begin
+    if (count == 4) count_in = 0;
+    else begin if (state == send || req)
+      count_in = count_r + 1;
+    else
+      count_in = 0;
+    end
+  end
+
+  always_ff @(posedge clk, negedge rst_b) begin
+    if (~rst_b)
+      count_r <= 0;
+    else
+      count_r <= count_in;
+  end
+
+  logic done;
+
+  assign count = count_r,
+         done = (state == send && count == 4),
+         payload_avail = (count > 0 && count < 5),
+         read_in = (state == hold || done);
+
+endmodule
+
+module serialToPkt(clk, rst_b, payload, put, pkt, pkt_avail, free);
+  parameter WIDTH = 32;
+  input bit clk, rst_b, put;
+  input bit [7:0] payload;
+  output bit pkt_avail, free;  
+  output logic [31:0] pkt;
+
+  bit [31:0] hold_in;
+  bit [1:0] count_in;
+  bit free_in;
+  
+  reg [1:0] count;
+  reg [31:0] hold;
+  reg free_r;
+
+  assign free_in = ~put; 
+  assign count_in = (put) ? count + 1 : 0;
+  assign hold_in = (put) ? (payload | (hold << 8)) : 0;
+
+  always_ff @(posedge clk, negedge rst_b) begin
+    if (~rst_b) begin
+        count <= 0;
+        free_r <= 1;
+    end
+    else begin
+        count <= count_in;
+        hold <= hold_in;
+        free_r <= free_in;
+    end
+  end
+
+  assign pkt = hold;
+  assign pkt_avail = (~free && ~put);
+  assign free = free_r;
+
+endmodule
+
+/*  Create a fifo (First In First Out) with depth 4 using the given interface
+ *  and constraints.
+ *  -The fifo is initally empty.
+ *  -Reads are combinational, so "data_out" is valid unless "empty" is asserted.
+ *   Removal from the queue is processed on the clock edge.
+ *  -Writes are processed on the clock edge.  
+ *  -If the "we" happens to be asserted while the fifo is full, do NOT update
+ *   the fifo.
+ *  -Similarly, if the "re" is asserted while the fifo is empty, do NOT update
+ *   the fifo. 
+ */
+
+module fifo(clk, rst_b, data_in, we, re, full, empty, data_out);
+  parameter WIDTH = 32;
+  input clk, rst_b;
+  input [WIDTH-1:0] data_in;
+  input we; //write enable
+  input re; //read enable
+  output full;
+  output empty;
+  output bit [WIDTH-1:0] data_out;
+
+  reg [3:0][WIDTH-1:0] Q;
+  reg [1:0] w_ptr, r_ptr;
+  reg [2:0] count;
+
+  assign full = (count == 3'd4),
+         empty = (count == 3'd0);
+  
+  always_ff @(posedge clk, negedge rst_b) begin
+    if (~rst_b) begin
+      count <= 0; w_ptr <= 0; r_ptr <= 0; Q <= 0;
+    end
+    else begin
+      if (re && we && count == 1) begin 
+         Q[w_ptr] <= data_in;
+         count <= count;
+         w_ptr <= w_ptr + 1;
+         r_ptr <= r_ptr + 1;
+      end else begin
+      if (we && !full) begin
+        Q[w_ptr] <= data_in;
+        count <= count + 1;
+        w_ptr <= w_ptr + 1;
+      end
+      if (re && !empty) begin
+        count <= count - 1;
+        r_ptr <= r_ptr + 1;
+      end
+    end
+    end
+  end  
+
+  assign data_out = Q[r_ptr];
+
+endmodule
---
+++ router.sv
@@ -0,0 +1,322 @@
+/*********************************************
+ *  18-341 Fall 2015                         *
+ *  Project 3                                *
+ *  Network-on-Chip router                   *
+ *********************************************/
+/*
+ * A router transfers packets between nodes and other routers.
+ */
+module router(clk, rst_b,
+              free_outbound, put_outbound, payload_outbound,
+              free_inbound, put_inbound, payload_inbound);
+  parameter ROUTERID = 0; // To differentiate between routers
+  input  clk, rst_b;
+
+  // self -> destination (sending a payload)
+  input [3:0] free_outbound;
+  output [3:0] put_outbound;
+  output [3:0][7:0] payload_outbound;
+
+  // source -> self (receiving a payload)
+  output [3:0] free_inbound;
+  input [3:0] put_inbound;
+  input [3:0][7:0] payload_inbound;
+
+  // internal data
+  pkt_t [3:0] outbound_pkts;         // from out_buffer, to node
+  logic [3:0] outbound_pkts_avail;
+  pkt_t [3:0] inbound_pkts;          // from node, to channel_sel
+  logic [3:0] inbound_pkts_avail;
+  logic [3:0] channel_req;           // request to in_buffer from channel_sel
+  logic [3:0] load_out_ready;        // tell routing outbuffer couldn't read
+
+  // io buffers
+  generate
+    for (genvar i = 0; i < 4; i++) begin: io
+      io_buffer iob (.clk(clk), .rst_b(rst_b),
+                 .payload_in(payload_inbound[i]),
+                 .payload_in_avail(put_inbound[i]),
+                 .payload_out(payload_outbound[i]),
+                 .payload_out_avail(put_outbound[i]),
+                 .pkt_in(outbound_pkts[i]),
+                 .pkt_in_avail(outbound_pkts_avail[i]),
+                 .pkt_out(inbound_pkts[i]),
+                 .pkt_out_avail(inbound_pkts_avail[i]),
+                 .port_in_q_ready(free_inbound[i]),
+                 .port_out_q_ready(load_out_ready[i]),
+                 .node_in_ready(free_outbound[i]),
+                 .read_in(channel_req[i]));
+    end
+  endgenerate
+
+  // packet handling and routing:
+  // IN_BUFFERS --> CHANNEL_SEL --> ROUTING --> OUT_BUFFERS
+  pkt_t pkt_to_route;              // CHANNEL_SEL --> ROUTING
+  logic pkt_to_route_avail;         
+  logic last_routed;               // ROUTING --> CHANNEL_SEL
+
+  routing rt (.clk(clk), .rst_b(rst_b),
+              .pkt_in(pkt_to_route), .pkt_in_avail(pkt_to_route_avail),
+              .out_read(load_out_ready),
+              .last_routed(last_routed),
+              .out_data_avail(outbound_pkts_avail),
+              .out_data(outbound_pkts));
+  channel_sel cs (.clk(clk), .rst_b(rst_b),
+                  .pkt_in(inbound_pkts), .pkt_in_avail(inbound_pkts_avail),
+                  .last_routed(last_routed), .req(channel_req),
+                  .pkt_out(pkt_to_route), .pkt_out_avail(pkt_to_route_avail)); 
+
+endmodule
+
+/* **********************************************
+ * I/O BUFFERS
+ * **********************************************
+ */
+
+/*
+ * wrapper holding input and output buffers to be connected
+ * to channel_sel, routing, and node
+ */
+module io_buffer(clk, rst_b,
+                 payload_in, payload_in_avail,
+                 payload_out, payload_out_avail,
+                 pkt_in, pkt_in_avail,
+                 pkt_out, pkt_out_avail,
+                 port_in_q_ready, node_in_ready, read_in,
+                 port_out_q_ready);
+  input clk, rst_b,
+        pkt_in_avail, payload_in_avail,
+        node_in_ready, read_in;
+  input pkt_t pkt_in;
+  input [7:0] payload_in;
+  output port_out_q_ready, port_in_q_ready,
+         payload_out_avail, pkt_out_avail;
+  output pkt_t pkt_out;
+  output [7:0] payload_out;
+  
+  bit ob_empty, ob_q_full, ib_q_full, node_payload_ready;                       
+  in_buffer ib (.clk(clk), .rst_b(rst_b),
+                .payload(payload_in), .put(payload_in_avail),
+                .req(read_in), .pkt_out(pkt_out), .pkt_out_avail(pkt_out_avail),
+                .full(ib_q_full));
+  out_buffer ob (.clk(clk), .rst_b(rst_b),
+                 .pkt(pkt_in), .pkt_avail(pkt_in_avail),
+                 .payload_out(payload_out), .payload_out_avail(payload_out_avail),
+                 .full(ob_q_full), .req(node_in_ready));
+
+  assign port_out_q_ready = ~ob_q_full,
+         port_in_q_ready = ~ib_q_full;
+  
+endmodule 
+
+/*
+ * Recieves packets from a node, queues them, and passes queue
+ * data on to channel_sel (wrapper for node queueing modules)
+ */
+module in_buffer(clk, rst_b,
+                 payload, put, req,
+                 pkt_out, pkt_out_avail, full);
+  input bit clk, rst_b, put, req;
+  input bit [7:0] payload;
+  output bit pkt_out_avail, full;
+  output pkt_t pkt_out;
+
+  // output from serial to queue
+  pkt_t pkt;
+  logic pkt_avail;
+  logic q_empty, free;
+
+  serialToPkt stp (.*);
+  fifo q (.clk(clk), .rst_b(rst_b),
+          .data_in(pkt), .we(pkt_avail),
+          .re(req), .full(full), .empty(q_empty),
+          .data_out(pkt_out));
+
+  assign pkt_out_avail = ~q_empty;
+
+endmodule
+
+/*
+ * Recieves packets from routing, queues them, passes them on to node
+ */
+module out_buffer(clk, rst_b,
+                  pkt, pkt_avail, req, 
+                  payload_out, payload_out_avail, 
+                  full);
+  input clk, rst_b, pkt_avail, req;
+  input pkt_t pkt;
+  output [7:0] payload_out;
+  output payload_out_avail, full;
+
+  pkt_t pkt_out;
+  bit read_in, q_empty, pkt_out_avail;
+  assign pkt_out_avail = ~q_empty;
+
+  fifo q (.clk(clk), .rst_b(rst_b),
+          .data_in(pkt), .we(pkt_avail),
+          .re(read_in), .full(full), .empty(q_empty),
+          .data_out(pkt_out));
+  pktToSerial pts (.clk(clk), .rst_b(rst_b),
+                   .pkt(pkt_out), .pkt_avail(pkt_out_avail),
+                   .payload_out(payload_out),
+                   .payload_avail(payload_out_avail),
+                   .read_in(read_in), .req(req));
+ 
+endmodule
+
+/* **********************************************
+ * ROUTING
+ * **********************************************
+ */
+
+/*
+ * Decides which packet from inputs to use
+ * - constantly see which nodes have packets available
+ * - request new packet when last one has been sent (1cycle)
+ */
+module channel_sel(clk, rst_b,
+                   pkt_in, pkt_in_avail, last_routed,
+                   pkt_out, pkt_out_avail, req);
+  parameter ROUTERID = 0;
+  input clk, rst_b;
+  input pkt_t [3:0] pkt_in;
+  input [3:0] pkt_in_avail;
+  input last_routed;
+  output pkt_t pkt_out;
+  output pkt_out_avail;
+  output [3:0] req;
+
+  // round-robin-esque scheduling, starting from 0
+  logic [3:0] use_p;   // one-hot port use
+  reg [3:0][3:0] p_wait;
+  reg [3:0] p_used;
+
+  always_comb begin // the always_comb from heeeellllll............
+    if (pkt_in_avail == 3'b0) use_p = 3'b0;
+    if (pkt_in_avail[0] == 1'b1) 
+      use_p[0] = (p_wait[0] >= p_wait[1] 
+                  && p_wait[0] >= p_wait[2]
+                  && p_wait [0] >= p_wait[3]) ? 1'b1 : 1'b0;
+    if (pkt_in_avail[1] == 1'b1)
+      use_p[1] = (p_wait[1] > p_wait[0]
+                  && p_wait[1] >= p_wait[2]
+                  && p_wait[1] >= p_wait[3]) ? 1'b1 : 1'b0;
+    if (pkt_in_avail[2] == 1'b1)
+      use_p[2] = (p_wait[2] > p_wait[0]
+                  && p_wait[2] > p_wait[1]
+                  && p_wait[2] >= p_wait[3]) ? 1'b1 : 1'b0;
+    if (pkt_in_avail[3] == 1'b1)
+      use_p[3] = (p_wait[3] > p_wait[0]
+                  && p_wait[3] > p_wait[1]
+                  && p_wait[3] > p_wait[2]) ? 1'b1 : 1'b0;
+   end
+
+  always_ff @(posedge clk, negedge rst_b) begin  
+    // increment wait count unless invalid or last used
+    if (~rst_b) p_wait = 0;
+    else begin
+      p_wait[0] = (p_used[0] || !pkt_in_avail[0]) ? 0 : p_wait[0] + 1;
+      p_wait[1] = (p_used[1] || !pkt_in_avail[1]) ? 0 : p_wait[1] + 1;
+      p_wait[2] = (p_used[2] || !pkt_in_avail[2]) ? 0 : p_wait[2] + 1;
+      p_wait[3] = (p_used[3] || !pkt_in_avail[3]) ? 0 : p_wait[3] + 1;
+    end
+  end
+
+  // hold last use_p val to reset p_wait at next cycle
+  always_ff @(posedge clk, negedge rst_b) begin
+    if (~rst_b) p_used <= 0;
+    else p_used <= use_p;
+  end
+
+  // store new packet when the last one has been sent
+  reg [31:0] bfr;
+  pkt_t channel;
+
+  always_comb
+    case(use_p)
+      4'b0001: channel = pkt_in[0];
+      4'b0010: channel = pkt_in[1];
+      4'b0100: channel = pkt_in[2];
+      4'b1000: channel = pkt_in[3];
+      default: channel = 0;
+   endcase 
+
+  always_ff @(posedge clk, negedge rst_b) begin
+    if (~rst_b) bfr <= 0;
+    else bfr <= (last_routed) ? channel : bfr;
+  end
+
+  // packet is available whenever buf has a value
+  assign pkt_out_avail = (0),
+         pkt_out = bfr;
+
+  assign req = (last_routed) ? use_p : 0;
+
+endmodule
+
+/*
+ * Gets a packet and pushes it and a valid bit to the an array of 4 pkts
+ * - register holds pkt until recieved signal?
+ */
+module routing(clk, rst_b,
+               pkt_in, pkt_in_avail, out_read,
+               out_data, out_data_avail, last_routed);
+  parameter ROUTERID = 0;
+  input clk, rst_b, pkt_in_avail;
+  input pkt_t pkt_in;
+  input [3:0] out_read;                   // one-hot indication that packet went through
+  output last_routed;
+  output bit [3:0] out_data_avail;
+  output pkt_t [3:0] out_data;
+
+  logic [2:0] send_node;
+  out_node #(ROUTERID) (.*);
+
+  // indicate whether the packet last sent got through
+  reg read;
+  always_ff @(posedge clk, negedge rst_b) begin
+    if (~rst_b) read <= 0;
+    else        read <= (out_read != 0) ? 1'b1 : 1'b0;
+  end
+
+  // assign outbound vals
+  assign last_routed = read;
+
+  always_comb
+    case(send_node)
+      0: begin
+         out_data = {32'b0, 32'b0, 32'b0, pkt_in};
+         out_data_avail = {1'b0, 1'b0, 1'b0, pkt_in_avail};
+         end
+      1: begin
+         out_data = {32'b0, 32'b0, pkt_in, 32'b0};
+         out_data_avail = {1'b0, 1'b0, pkt_in_avail, 1'b0};
+         end
+      2: begin
+         out_data = {32'b0, pkt_in, 32'b0, 32'b0};
+         out_data_avail = {1'b0, pkt_in_avail, 1'b0, 1'b0};
+         end
+      3: begin 
+         out_data = {pkt_in, 32'b0, 32'b0, 32'b0};
+         out_data_avail = {pkt_in_avail, 1'b0, 1'b0, 1'b0};
+         end
+    endcase
+
+endmodule
+
+/*
+ * Takes in a packet, determines which node to send it to.
+ */
+module out_node(clk, rst_b,
+               pkt_in, pkt_in_avail,
+               send_node);
+  parameter ROUTERID = 0;
+  input clk, rst_b, pkt_in_avail;
+  input pkt_t pkt_in;
+  output [2:0] send_node;
+
+  logic [3:0] dest;
+  assign dest = pkt_in.sourceID,
+         send_node = (dest == ROUTERID) ? dest + 1 : 0; // TODO: fix this to work for both router ids!!
+
+endmodule
